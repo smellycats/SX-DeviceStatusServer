69815dd4c2b03bee60aabd23cbd80cda34e687e7
SmellyCat <smellycat2014@foxmail.com>
2017-03-15 15:16:09 +0800

    更改 models测试方法

--- models_test.py
+++ models_test.py
@@ -18,10 +18,26 @@ def test_user_get():
     print user.scope
     
 def test_device_get():
-    dev = db.session.query(Device).filter(
-        Device.modified <= arrow.now().replace(minutes=-1).datetime).order_by(Device.modified).limit(3).all()
-    print dev
+##    dev = db.session.query(Device).filter(
+##        Device.modified <= arrow.now().replace(minutes=-1).datetime).order_by(Device.modified).limit(3).all()
+##    print dev
+    #now = arrow.utcnow().replace(hours=8)
+    dev_list = db.session.query(Device).filter(
+        Device.modified <= arrow.now('PRC').replace(minutes=-1).datetime.replace(tzinfo=None),
+        Device.last_access <= arrow.now('PRC').replace(minutes=-1).datetime.replace(tzinfo=None),
+        Device.banned==0).order_by(Device.modified).limit(10).all()
+    print dev_list
+    #now = arrow.now('PRC').datetime
+    #print now
+    for i in dev_list:
+        i.last_access = arrow.now('PRC').datetime.replace(tzinfo=None)
+    db.session.commit()
 
+def test_device_get2():
+    dev = db.session.query(Device).filter(
+        Device.ip == '192.168.189.147').first()
+    print type(dev.last_access)
+    print dev.last_access
 
 def test_device_post():
     ip = '192.168.186.186123'
@@ -41,5 +57,6 @@ def test_device_post():
 
 if __name__ == '__main__':
     test_device_get()
-    test_device_post()
+    #test_device_get2()
+    #test_device_post()
 
